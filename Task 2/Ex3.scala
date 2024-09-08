827e0add27b983bb067b035052c72ec42a5930b7
Mudassir Ghulam Sarwar <89977846+CherrySMO@users.noreply.github.com>
2024-09-09 01:21:00 +0500

    Update Ex3.scala

--- Task 2/Ex3.scala
+++ Task 2/Ex3.scala
@@ -1,3 +1,4 @@
+object Ex3{
 def elementwiseSum(arr1: Array[Double], arr2: Array[Double]): Array[Double] = {
   (arr1, arr2).zipped.map(_ + _)
 }
@@ -6,4 +7,6 @@ val arr1 = Array(3.5, 2.4, 9.7 )
 val arr2 = Array(2.20, 3.0, 3.55)
 
 val result = elementwiseSum(arr1, arr2)
-println(result.mkString(", "))  // Output: 5.0, 7.0, 9.0
+println(result.mkString(", "))
+
+}
