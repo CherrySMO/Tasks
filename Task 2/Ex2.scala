fb67f9e827ae715a4105c62e1ce0b04666c10d3d
Mudassir Ghulam Sarwar <89977846+CherrySMO@users.noreply.github.com>
2024-09-11 01:53:05 +0500

    Delete Task 2 directory

--- Task 2/Ex2.scala
+++
@@ -1,9 +0,0 @@
-object Ex2{
-  def charArray(start: Int, accumalation: List[Char] = List()): List[Char] = {
-  if (start > 122) accumalation // 98 is the ASCII code for "z" 
-  else charArray(start + 1, accumalation :+ start.toChar) 
-}
-val result = charArray(98) // 98 is the ASCII code for "a"
-println(result)
-
-}
